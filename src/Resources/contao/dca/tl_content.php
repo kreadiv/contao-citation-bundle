f805f0e171497569dfc0b20ecd31250f9ef6c0c5
Florian Wenzel <info@wenzel-webdesign.de>
2020-06-25 22:09:24 +0200

    Array syntax changed

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -4,18 +4,18 @@
 $GLOBALS['TL_DCA']['tl_content']['palettes']['citation'] = '{type_legend},type,citation,citation_src;{protected_legend:hide},protected;{expert_legend:hide},guests,invisible,cssID,space';
 
 // Fields
-$GLOBALS['TL_DCA']['tl_content']['fields']['citation'] = array(
+$GLOBALS['TL_DCA']['tl_content']['fields']['citation'] = [
     'label'       => &$GLOBALS['TL_LANG']['tl_content']['citation'],
     'exclude'   => true,
     'search'    => true,
     'inputType' => 'textarea',
     'eval'      => array('mandatory' => true, 'tl_class' => 'long', 'style' => 'height:5em;')
-);
+];
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['citation_src'] = array(
+$GLOBALS['TL_DCA']['tl_content']['fields']['citation_src'] = [
     'label'       => &$GLOBALS['TL_LANG']['tl_content']['citation_src'],
     'exclude'   => true,
     'search'    => true,
     'inputType' => 'text',
     'eval'      => array('maxlength' => 255, 'tl_class' => 'long')
-);
+];
