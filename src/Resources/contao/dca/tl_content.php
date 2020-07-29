5201a20695463224dcadb9a9e1f89cebd61a0b4c
Florian Wenzel <mail@kreadiv.de>
2020-07-29 10:22:12 +0200

    Add cite source url

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,7 +1,7 @@
 <?php
 
 // Palettes
-$GLOBALS['TL_DCA']['tl_content']['palettes']['citation'] = '{type_legend},type,citation,citation_src;{protected_legend:hide},protected;{expert_legend:hide},guests,invisible,cssID,space';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['citation'] = '{type_legend},type,citation,citation_src_text,citation_src_url;{protected_legend:hide},protected;{expert_legend:hide},guests,invisible,cssID,space';
 
 // Fields
 $GLOBALS['TL_DCA']['tl_content']['fields']['citation'] = [
@@ -13,11 +13,20 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['citation'] = [
     'sql'       => 'mediumtext NULL'
 ];
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['citation_src'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_content']['citation_src'],
+$GLOBALS['TL_DCA']['tl_content']['fields']['citation_src_text'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_content']['citation_src_text'],
     'exclude'   => true,
     'search'    => true,
     'inputType' => 'text',
-    'eval'      => ['maxlength' => 255, 'tl_class' => 'long'],
+    'eval'      => ['maxlength' => 255, 'tl_class' => 'w50'],
+    'sql'       => 'varchar(255) NULL'
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['citation_src_url'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_content']['citation_src_url'],
+    'exclude'   => true,
+    'search'    => true,
+    'inputType' => 'url',
+    'eval'      => ['maxlength' => 255, 'tl_class' => 'w50'],
     'sql'       => 'varchar(255) NULL'
 ];
